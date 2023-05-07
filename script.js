e71c80d4867e83c987071bbcc00472fd47bf2ca6
wiliancrosa <117321491+wiliancrosa@users.noreply.github.com>
2023-05-07 22:55:11 +0100

    Add files via upload
    alinhamento dos cards tv

--- script.js
+++ script.js
@@ -13,7 +13,23 @@ let slider = tns({
       fixedWidth: 280,
       gutter: 0,
       fixedHeight: true,
-      items: 1,
+      items: 3,
     },
   },
+});
+
+// Seleciona todos os botões de navegação
+const botaoNavegacao = document.querySelectorAll("a.botao-navegacao");
+
+// Para cada botão de navegação, adicione um evento de clique
+botaoNavegacao.forEach((botao) => {
+  botao.addEventListener("click", (e) => {
+    e.preventDefault(); // Previne o comportamento padrão do link
+
+    // Seleciona o id da sessão correspondente ao botão de navegação clicado
+    const idSessao = botao.getAttribute("href");
+
+    // Seleciona a sessão correspondente usando o id e rola a página para ela
+    document.querySelector(idSessao).scrollIntoView({ behavior: "smooth" });
+  });
 });
